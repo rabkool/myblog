2a070bbc666e3be72c712dfe31d5f0ed80066fb6
rabkool <905956347@qq.com>
2019-05-17 13:05:40 +0800

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+#!usr/bin/env pyhton
+# -*- coding: UTF-8 -*-
+# Author:Yin
+from django.urls import path, include
+
+import blog.views
+
+
+urlpatterns = [
+    path('hello_world', blog.views.index),
+    path('content', blog.views.article_content),
+    path('index', blog.views.get_index_page),
+    # path('detail', blog.views.get_detail_page)
+    path('detail/<int:article_id>', blog.views.get_detail_page)
+]
